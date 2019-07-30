330112d2cd5a519212970139b553dc68b084b2eb
Welison Almeida <welison.almeida.923@gmail.com>
2019-07-29 22:26:26 -0300

    Add current c++ template.

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+inline void FAST_IO(){ ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+
+#define pb push_back
+#define ff first
+#define ss second
+#define len(a) (int)(a).size()
+#define all(x) x.begin(), x.end()
+#define debug(x) cout << "> x:" << x << endl;
+
+int main() {
+    FAST_IO();
+
+    return 0;
+}
