fcd02efdbaf387b191646cd011229fa8402a50b0
Welison Almeida <welison.almeida.923@gmail.com>
2020-05-29 09:12:26 -0300

    Add permutation backtracking problem.

--- book_algorithms/backtracking/permutation.cpp
+++ book_algorithms/backtracking/permutation.cpp
@@ -2,6 +2,28 @@
 #define len(x) (int) (x.size())
 using namespace std;
 
+// https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+
+void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
+    if (begin == (int)num.size()) {
+        result.push_back(num);
+        return;
+    }
+    
+    for (int i = begin; i < (int)num.size(); i++) {
+        swap(num[begin], num[i]);
+        permuteRecursive(num, begin + 1, result);
+        swap(num[begin], num[i]);
+    }
+}
+
+vector<vector<int>> permute(vector<int> &num) {
+    vector<vector<int>> result;
+    
+    permuteRecursive(num, 0, result);
+    return result;
+}
+
 int main(){
     vector<int> set {3, 2, 1};
     sort(set.begin(), set.end());
@@ -13,5 +35,16 @@ int main(){
         cout << '\n';
     } while(next_permutation(set.begin(), set.end()));
     // Rearranges the elements in the range [first,last) into the next lexicographically greater permutation.
+
+    cout << "--------------------" << '\n';
+
+    vector<int> case1 {1, 2, 3};
+    vector<vector<int>> ans = permute(case1);
+    for(int i = 0; i < (int)ans.size(); i++) {
+        for(int j = 0; j < (int)ans[i].size(); j++) {
+            cout << ans[i][j] << ' ';
+        }
+        cout << '\n';
+    }
     return 0;
 }
