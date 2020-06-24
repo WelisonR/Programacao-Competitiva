43ede683bdc6e5fa08c1a210e8ff456c4b618465
Welison Almeida <welison.almeida.923@gmail.com>
2020-06-24 20:47:04 -0300

    Add sieve algorithm.

---
+++ math/sieve_eratosthenes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Computa os primos menores ou iguais a n
+vector<int> sieve(int n) {
+    // sieve_v[i] = 0 denota que o i-ésimo número é primo
+    vector<int> sieve_v(n+1, 0);
+    sieve_v[0] = -1; sieve_v[1] = 1;
+
+    for(int i = 2; i*i <= n; i++) {
+        if (sieve_v[i]) continue;
+
+        for(int j = i*i; j <= n; j += i)
+            sieve_v[j] = i; // i é um fator de j
+    }
+
+    return sieve_v;
+}
+
+int main(){
+    int num = 933217;
+    vector<int> primes = sieve(933217);
+    
+    cout << "O número " << num << (primes[num] == 0 ? " é primo!" : "não é primo!") << '\n';
+    cout << primes[102] << '\n';
+
+    return 0;
+}
